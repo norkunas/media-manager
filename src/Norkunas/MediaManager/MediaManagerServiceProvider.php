ee346c9cea87e11bd524a39244a63d8323a425d4
Tomas Norkūnas <norkunas.tom@gmail.com>
2014-06-28 18:02:18 +0300

    f

--- src/Norkunas/MediaManager/MediaManagerServiceProvider.php
+++ src/Norkunas/MediaManager/MediaManagerServiceProvider.php
@@ -4,28 +4,28 @@ namespace Norkunas\MediaManager;
 use Illuminate\Support\ServiceProvider;
 
 class MediaManagerServiceProvider extends ServiceProvider {
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot() {
-		$this->package('norkunas/media-manager');
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot() {
+        $this->package('norkunas/media-manager');
 
         include __DIR__ . '/../../routes.php';
-	}
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register() {
+    }
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register() {
         $this->app['mediamanager'] = $this->app->share(function($app) {
             return new MediaManager();
         });
@@ -34,13 +34,13 @@ class MediaManagerServiceProvider extends ServiceProvider {
             $loader = \Illuminate\Foundation\AliasLoader::getInstance();
             $loader->alias('MediaManager', 'Norkunas\MediaManager\Facades\MediaManager');
         });
-	}
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides() {
-		return array();
-	}
+    }
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides() {
+        return array();
+    }
 }
